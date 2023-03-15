95b9852c831d64b33a73c50db29154157fd4889e
Ihavenolife <matuswall@gmail.com>
2023-03-15 18:23:31 +0100

    build

---
+++ build_win.ps1
@@ -0,0 +1,1 @@
+pyinstaller --onefile --noconsole --add-data "./previous.png;."  --add-data "./pause.png;." --add-data "./next.png;." --add-data "./play.png;." main.py
--- classes/images.py
+++ classes/images.py
@@ -1,7 +1,18 @@
+import os
+import sys
+
 from PIL import ImageTk, Image
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 class Images:
-    previous = ImageTk.PhotoImage(Image.open("images/previous.png"))
-    pause = ImageTk.PhotoImage(Image.open("images/pause.png"))
-    next = ImageTk.PhotoImage(Image.open("images/next.png"))
-    play = ImageTk.PhotoImage(Image.open("images/play.png"))
+    previous = ImageTk.PhotoImage(Image.open(resource_path("previous.png")))
+    pause = ImageTk.PhotoImage(Image.open(resource_path("pause.png")))
+    next = ImageTk.PhotoImage(Image.open(resource_path("next.png")))
+    play = ImageTk.PhotoImage(Image.open(resource_path("play.png")))
